18e106d49e476a1009b7d3a69e1580c1eab56153
PyCraftMusadaq <musadaqtanveer7865@gmail.com>
2025-05-06 21:54:19 +0500

    add detail view

--- BaseViewCode/myapp/urls.py
+++ BaseViewCode/myapp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("myclassiew2/",views.MyclassView2.as_view(),name="myclassiew2"),
     path("home/<int:pk>/",views.HomePageView.as_view(extra_context={'course':"Python"}),name="home"),
     path("profile/<int:id>/",views.ProfilePageView.as_view(),name="profile"),
+    path("<int:pk>/",views.SingleStudentView.as_view(),name="profile"),
+    path("student/<int:pk>/",views.StudentDetailView.as_view(),name="studentdetail"),
 ]
--- BaseViewCode/myapp/views.py
+++ BaseViewCode/myapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views import View 
 from django.views.generic.base import TemplateView,RedirectView
-
+from django.views.generic.detail import DetailView
+from .models import Student
 # Create your views here.
 
 class MyclassView1(View):
     def get(self,request):
         return render(request,"home.html")
-    
+        
     
 class MyclassView2(View):
     name = "Musadaq"
@@ -52,12 +53,36 @@ class ProfilePageView(TemplateView):
 
 
 class MyDefaultView(RedirectView):
-    # url = 'home'
-    pattern_name = 'home'
+    url = 'home'
+    #pattern_name = 'home'
     query_string = True # if we want to get query parameters passed in url 
-    # url = 'https://goog.com  # external urls can also be used 
+    # url = 'https://google.com  # external urls can also be used 
     # one of them can be use to redirect from this view to target view 
     # if we want to pass arguments in url then we can use get_redirect_url function and extract **kwargs
     def get_redirect_url(self, *args, **kwargs):
         print(kwargs)
         return super().get_redirect_url(*args, **kwargs)
+    
+# Base View for single details    
+class SingleStudentView(View):
+    def get(self,request,pk):
+        single_student = Student.objects.get(pk=pk)
+        return render(request,"profile.html",{'student':single_student})
+      
+# Detail View
+class StudentDetailView(DetailView):
+    # model from we are getting detail of instance
+    model = Student
+    template_name = 'profile.html'
+    # if we change the slug from pk to another we will use pk_url_kwarg
+    #pk_url_kwarg = 'id' # 
+    #template_name_suffix = "_detail"
+    # context name is same as model name like student in our case but if we want to chang we can as usual we did 
+    context_object_name = 'student'
+    # if we want to pass aditional data we can use this method
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['musadaq'] = Student.objects.all().order_by('name')
+        return context 
+        
+            
